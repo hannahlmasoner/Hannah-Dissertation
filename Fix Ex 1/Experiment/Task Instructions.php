6497f1f2db387f8c5ffeb5fb7a09868e137049c4
npm27 <nick.maxwell61@yahoo.com>
2021-03-04 11:05:19 -0600

    Build test experimient

---
+++ Fix Ex 1/Experiment/Task Instructions.php
@@ -0,0 +1,24 @@
+<section class="instructions">
+    <h2 class="textcenter">Task Instructions</h2>
+    <!-- ## SET ## Change the instructions text to match your task.-->
+    <p>In this study you will be watching some videos and then telling us what you see.
+        </p>
+    <p>Please pay close attention to what is being shown.
+    <!--<p> Time for you to move onto the experiment. </p>-->
+
+    <div class="textcenter" id="revealRC">
+        <button class="collectorButton" type="button">Advance</button>
+    </div>
+</section>
+
+<!-- ## SET ## This ensures that participants read your instructions.
+     Participants must correctly answer something about the procedure -->
+<div class="readcheck">
+    Should you pay close attention?  (hint: Answer is in the instructions)
+    <ul>
+        <li class="MCbutton"             > I don't think so </li>
+        <li class="MCbutton"             > Nope             </li>
+        <li class="MCbutton" id="correct"> Yes              </li>
+        <li class="MCbutton"             > I can't read.    </li>
+    </ul>
+</div>
